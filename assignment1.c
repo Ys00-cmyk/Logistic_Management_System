ee6395f9b76eb189a3c554e2641111bc84331d12
Ys00-cmyk <sewminiyasodara100@gmail.com>
2025-10-24 18:23:08 +0530

    Update assignment1.c
    added delivery record structure and modify delivery processing function to save records

--- assignment1.c
+++ assignment1.c
@@ -11,6 +11,22 @@ int city_count = 0;
 int distances[MAX_CITIES][MAX_CITIES];
 
 
+struct DeliveryRecord {
+    int from_city;
+    int to_city;
+    int weight;
+    int vehicle_type;
+    int distance;
+    float cost;
+    float time;
+    float fuel_cost;
+    float customer_charge;
+};
+
+
+struct DeliveryRecord deliveries[MAX_DELIVERIES];
+int delivery_count = 0;
+
 struct Vehicle {
     char name[20];
     int capacity;
@@ -18,8 +34,6 @@ struct Vehicle {
     int avg_speed;
     int fuel_efficiency;
 };
-
-
 struct Vehicle vehicles[3] = {
     {"Van", 1000, 30, 60, 12},
     {"Truck", 5000, 40, 50, 6},
@@ -142,7 +156,104 @@ void calculate_delivery_cost() {
     }
 
     display_cities();
+void process_delivery() {
+    if (city_count < 2) {
+        printf("Need at least 2 cities for delivery calculation!\n");
+        return;
+    }
 
+    if (delivery_count >= MAX_DELIVERIES) {
+        printf("Maximum delivery limit reached!\n");
+        return;
+    }
+
+    display_cities();
+
+    int from, to, weight, vehicle_type;
+    printf("Enter source city number: ");
+    scanf("%d", &from);
+    printf("Enter destination city number: ");
+    scanf("%d", &to);
+
+    from--; to--;
+
+    if (from < 0 || from >= city_count || to < 0 || to >= city_count) {
+        printf("Invalid city numbers!\n");
+        return;
+    }
+
+    if (from == to) {
+        printf("Source and destination cannot be same!\n");
+        return;
+    }
+
+    if (distances[from][to] == -1) {
+        printf("Distance not set between these cities!\n");
+        return;
+    }
+
+    printf("Enter weight (kg): ");
+    scanf("%d", &weight);
+
+    display_vehicles();
+    printf("Select vehicle type (1-3): ");
+    scanf("%d", &vehicle_type);
+    vehicle_type--;
+
+    if (vehicle_type < 0 || vehicle_type > 2) {
+        printf("Invalid vehicle type!\n");
+        return;
+    }
+
+    if (weight > vehicles[vehicle_type].capacity) {
+        printf("Weight exceeds vehicle capacity!\n");
+        return;
+    }
+
+
+    int distance = distances[from][to];
+    float base_cost = distance * vehicles[vehicle_type].rate_per_km * (1 + (float)weight / 10000);
+    float delivery_time = (float)distance / vehicles[vehicle_type].avg_speed;
+    float fuel_used = (float)distance / vehicles[vehicle_type].fuel_efficiency;
+    float fuel_cost = fuel_used * 310;
+    float total_operational_cost = base_cost + fuel_cost;
+    float profit = base_cost * 0.25;
+    float customer_charge = total_operational_cost + profit;
+
+
+    deliveries[delivery_count].from_city = from;
+    deliveries[delivery_count].to_city = to;
+    deliveries[delivery_count].weight = weight;
+    deliveries[delivery_count].vehicle_type = vehicle_type;
+    deliveries[delivery_count].distance = distance;
+    deliveries[delivery_count].cost = base_cost;
+    deliveries[delivery_count].time = delivery_time;
+    deliveries[delivery_count].fuel_cost = fuel_cost;
+    deliveries[delivery_count].customer_charge = customer_charge;
+
+    delivery_count++;
+
+
+    printf("\n======================================================\n");
+    printf("DELIVERY COST ESTIMATION\n");
+    printf("------------------------------------------------------\n");
+    printf("From: %s\n", cities[from]);
+    printf("To: %s\n", cities[to]);
+    printf("Distance: %d km\n", distance);
+    printf("Vehicle: %s\n", vehicles[vehicle_type].name);
+    printf("Weight: %d kg\n", weight);
+    printf("------------------------------------------------------\n");
+    printf("Base Cost: %.2f LKR\n", base_cost);
+    printf("Fuel Used: %.2f L\n", fuel_used);
+    printf("Fuel Cost: %.2f LKR\n", fuel_cost);
+    printf("Operational Cost: %.2f LKR\n", total_operational_cost);
+    printf("Profit: %.2f LKR\n", profit);
+    printf("Customer Charge: %.2f LKR\n", customer_charge);
+    printf("Estimated Time: %.2f hours\n", delivery_time);
+    printf("======================================================\n");
+
+    printf("Delivery record saved successfully!\n");
+}
     int from, to, weight, vehicle_type;
     printf("Enter source city number: ");
     scanf("%d", &from);
